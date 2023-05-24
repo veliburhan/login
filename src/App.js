fce261ad77ca57c2188793bcd2a56f13d74de477
veli.burhan <byvely@gmail.com>
2023-05-24 12:47:16 +0300

    Hizmet Bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { HashRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container py-3">
+      <Header/>
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path="login" element={<Login/>}/>
+        </Routes>
+      </HashRouter>
+      
+      <Footer/>
+
+      </div> );
+
+
 }
 
-export default App;
+      export default App;
